f2d936d41f22ebac0995b5d5093e127eef4bcd08
phm0423 <qkrgusalsok@gmail.com>
2025-05-20 12:35:21 +0900

    9일차 오전 커밋

--- README.md
+++ README.md
@@ -466,5 +466,24 @@ IoT 개발자 WPF 학습리포지토리 2025
 ## 9일차 
 
 ### 스마트홈 연동 모니터링앱 (계속)
+- MQTT부터 시작
+
+#### 네트워크 확인
+- telnet 명령어로 서버서비스가 동작중인지 확인
+- telnet 아이피주소 포트번호
+    ```shell
+    # MySQL에 접속가능한지 여부
+    > telnet 127.0.0.1 3306
+    # MySQL에 접속가능한지 여부
+    > telnet 127.0.0.1 1883
+    ```
 
 #### MQTT 시뮬레이션 (계속)
+1. MqttPub.py 소스코드에 Fake IoT 센서값 전달 코딩
+2. Fake 센싱값을 json으로 Publish
+3. C# MahApps.Metro 사용 MQTT 데이터 Subscriber앱
+    - CommunityTollkit.Mvvm
+    - MahApps.Metro
+    - MahApps.Metro.IconPacks
+    - Newtonsoft.Json
+    - MQTTnet
---
+++ day09/Day09Study/WpfMqttSubApp/VieModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace WpfMqttSubApp.VieModels
+{
+    public partial class MainViewModel: ObservableObject
+    {
+        private readonly IDialogCoordinator dialogCoordinator;
+        private string _brokerHost;
+        private string _databaseHost;
+
+        // 속성 BrokerHost, DataHost
+        // 메서드 ConnectBoroker Command, ConnectDatabaseCommand
+
+        public MainViewModel(IDialogCoordinator coordinator) 
+        {
+            this.dialogCoordinator = coordinator;
+
+            BrokerHost = "211.119.12.55";
+            DatabaseHost = "211.199.12.55";
+        }
+
+        public string BrokerHost
+        {
+            get => _brokerHost;
+            set => SetProperty(ref _brokerHost, value);
+        }
+
+        public string DatabaseHost
+        {
+            get => _databaseHost;
+            set => SetProperty(ref _databaseHost, value);
+        }
+
+        [RelayCommand]
+        public async Task ConnectBroker()
+        {
+            await this.dialogCoordinator.ShowMessageAsync(this, "브로커연결", "브로커연결합니다!");
+        }
+
+        [RelayCommand]
+        public async Task ConnectDatabase()
+        {
+            await this.dialogCoordinator.ShowMessageAsync(this, "DB연결", "DB연결합니다!");
+
+        }
+    }
+}
